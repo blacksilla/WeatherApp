761909f611a852ab131928609f97474fcea752b6
blacksilla <afonsosilva1@ua.pt>
2023-02-28 17:54:54 +0000

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public IActionResult Results()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> WeatherApiForm(Weather cw)
+        {
+
+            //API
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + cw.LocationName + "?key=B4ELH6C3FVGXL7Q5WY5QJZMYP");
+            var response = await client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+                //lança um erro
+            var body = await response.Content.ReadAsStringAsync();
+
+            ViewBag.Weather = body;
+
+            return View("Results");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Weather.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public string locationName;
+        public Weather()
+        {
+            
+        }
+        
+        public string LocationName { get; set; }
+    }
+
+    
+}
